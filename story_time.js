5e87a51acc9c855461adf43f46fc246c315873c7
kshenouda123 <129615197+kshenouda123@users.noreply.github.com>
2024-07-17 12:35:56 -0600

    test

---
+++ story_time.js
@@ -0,0 +1,42 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greeting = "welcome to the obsticle course";
+let name = "Jimmy the jumper";
+let title = "judge";
+let obsticleName = "fire throwing race;"
+
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year = 3013;
+let age = 71;
+let century = 31;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let items = ['first aid kit', 'waterbottle', 'towel', 'swimming flippers'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let firstPlace = true; 
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+console.log(greeting + " my name is " + name + ".");
+console.log("as the previous winner of the " + obsticleName + " I am now the new " + title + ".");
+console.log("as I competed many years ago, I am now " + age + " years old." )
+console.log("sense then the course has been updated to use the " + year + " technology that was not previously avaiable");
+console.log("however, the challenges are vary similar even though you are competing in the " + century + "th century");
+console.log("does anyone have anything to ask?");
+console.log("Me: what items are we allowed to have in this challenge?");
+console.log("you are given a " + items[0] + ", " + items[1] + ", " + items[2] + ", and  " + items[3]);
+console.log("any other questions");
+console.log("let the race begin in");
+console.log(3 + "!");
+console.log(2 + "!");
+console.log(1 + "!");
+
+
+
+
